3e78ef60fe90dbc4ed515f72e252bc431ab1fe7c
dreeseh <1859@holbertonschool.com>
2020-10-18 20:53:22 +0000

    task 2 & big o answers

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,5 +6,41 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *current, *temp;
 
+	if (*list == NULL)
+	{
+		return;
+	}
+
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		temp = current->next;
+
+		while (current->prev && current->n < current->prev->n)
+		{
+			current->prev->next = current->next;
+
+			if (current->next != NULL)
+			{
+				current->next->prev = current->prev;
+
+			}
+			current->next = current->prev;
+			current->prev = current->next->prev;
+
+			if (current->prev != NULL)
+			{
+				current->prev->next = current;
+			}
+			else
+			{
+				*list = current;
+			}
+			current->next->prev = current;
+			print_list(*list);
+		}
+		current = temp;
+	}
 }
