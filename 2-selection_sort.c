c031d1617b41d86627aa95efac84722044ffc68b
dreeseh <1859@holbertonschool.com>
2020-10-19 14:34:01 +0000

    task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,5 +7,34 @@
  */
 void selection_sort(int *array, size_t size)
 {
+	unsigned long int i, j, index;
 
+	for (i = 0 ; i < size - 1 ; i++)
+	{
+		index = i;
+		for (j = i + 1 ; j < size ; j++)
+		{
+			if (array[j] < array[index])
+			{
+				index = j;
+			}
+		}
+		swap(&array[index], &array[i]);
+		print_array(array, size);
+	}
+}
+
+/**
+ * swap - swaps two integers
+ * @first: first int to be swapped
+ * @second: second int to be swapped
+ * Return: is void
+ */
+void swap(int *first, int *second)
+{
+	int temp;
+
+	temp = *first;
+	*first = *second;
+	*second = temp;
 }
