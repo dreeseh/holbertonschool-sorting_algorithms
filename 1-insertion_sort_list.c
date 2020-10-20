c38c88e63084726e5b8637e220c16477353f8933
dreeseh <1859@holbertonschool.com>
2020-10-20 19:11:38 +0000

    testing for nulls

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,7 +8,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *temp;
 
-	if (*list == NULL || list == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 	{
 		return;
 	}
