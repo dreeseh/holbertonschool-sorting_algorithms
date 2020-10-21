fbc8bced2efbee24651c7ea7f0c01ccfe760c9e8
dreeseh <1859@holbertonschool.com>
2020-10-21 17:21:43 +0000

    repush sort.h

--- sort.h
+++ sort.h
@@ -30,4 +30,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
+void shell_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void heapify(int *array, int min, int max, size_t size);
 #endif /** _SORT_HEADER_ **/
