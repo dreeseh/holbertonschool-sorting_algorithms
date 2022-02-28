ab6866a2d6ec28a52b0f8b2f3bd645339544f4f3
dreeseh <1859@holbertonschool.com>
2022-02-28 17:47:37 +0000

    all files for LSD Radix Sort

--- sort.h
+++ sort.h
@@ -35,4 +35,6 @@ void merge_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void heapify(int *array, int min, int max, size_t size);
+void radix_sort(int *array, size_t size);
+
 #endif /** _SORT_HEADER_ **/
