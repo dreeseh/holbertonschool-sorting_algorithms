0bafcdbf8c611b62e7a6834bc92ae126e50faced
dreeseh <1859@holbertonschool.com>
2023-02-15 17:21:41 +0000

    adding C file for tests

---
+++ 106-bitonic_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * @brief 
+ * 
+ */
+void _swap(int *left, int *right)
+{
+	int temp;
+
+	temp = *left;
+	*left = *right;
+	*right = temp;
+}
+
+/**
+ * @brief 
+ * 
+ */
+void _merge(int *array, int low, int count, int dir, size_t size)
+{
+	
+}
+
+/**
+ * @brief 
+ * 
+ */
+void bitonic_sort(int *array, size_t size)
+{
+
+}
